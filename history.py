f188a339ce3cb9e2a302cf20317b2642cc77eaa4
edwins-acct <edwins.acct@gmail.com>
2018-12-17 19:42:04 +0000

    add

--- history.py
+++ history.py
@@ -29,25 +29,27 @@ else:
 url= "https://api.iextrading.com/1.0/stock/" + str(qString.upper()) +"/batch?types=quote,chart&range=1m"
 response = requests.get(url) #gets info
 parse = urlparse(url) #prints> ParseResult(scheme='https', netloc='api.iextrading.com', path='/1.0/stock/market/batch', params='', query='symbols=V,FB,NBIX&types=quote', fragment='')
-#querystringl = parse_qsl(parse.query) #prints> [('symbols', 'V,FB,NBIX'), ('types', 'quote')]
 querystring = parse_qs(parse.query)  #prints> {'symbols': ['V,FB,NBIX'], 'types': ['quote']}
 parsed = json.loads(response.text) #json.loads will parse data from response.text, has option to format. ex: json.loads(response.text, indent=4)
 #print(json.dumps(parsed, indent=2))
 #print(len(parsed['chart']))
-#symbolsList=querystring['symbols'][0].split(',')
-#print(parsed['chart'][3]['changePercent'])
 #sys.exit()	
 
 print(f"{qString.upper():<10}")
-print(f"{'Date':>10}{'Close':>10}{'Change %':>10}{'Volume':>15}{'AvgVolume':>15}")
+print(f"{'Date':>10}{'Close':>10}{'Change %':>10}{'Volume':>12}{'AvgVol':>10}")
 t_price = f"{parsed['quote']['latestPrice']:.2f}".join('$ ')
-t_chgPct = f"{parsed['quote']['changePercent']:.2f}".join(' %') 
+x = str(parsed['quote']['changePercent'])
+if x.startswith("-"): 
+    t_chgPct = colored(f"{parsed['quote']['changePercent']:.2f}".join(' %'),'red')
+else:
+    t_chgPct = colored(f"{parsed['quote']['changePercent']:.2f}".join(' %'),'green')
 t_vol = parsed['quote']['latestVolume']
 t_vol1 = f"{parsed['quote']['latestVolume']:,}"
 t_avgVol = parsed['quote']['avgTotalVolume']
 t_chgVol = f"{(t_vol/t_avgVol):.0%}"
-print(f"{t_date:<10}{t_price:>10}{t_chgPct:>10}{t_vol1:>15}{t_chgVol:>15}")
-for i in range((len(parsed['chart'])-1),-1,-1):
+print(f"{t_date:<10}{t_price:>10}{t_chgPct:>20}{t_vol1:>12}{t_chgVol:>10}")
+#for i in range(0,len(parsed['chart'])): #in descending order
+for i in range((len(parsed['chart'])-1),-1,-1): #in ascending order
     date = f"{parsed['chart'][i]['date']}"
     close = f"{parsed['chart'][i]['close']:.2f}".join('$ ') 
     x = str(parsed['chart'][i]['changePercent'])
@@ -56,9 +58,10 @@ for i in range((len(parsed['chart'])-1),-1,-1):
     else:
         chgPct = colored(f"{parsed['chart'][i]['changePercent']:.2f}".join(' %'),'green')
     chgPct.strip 
+#    print(repr(chgPct))  #prints special characters;normally invisible
     vol= f"{parsed['chart'][i]['volume']:,}" 
     chgVol = f"{(parsed['chart'][i]['volume']/t_avgVol):.0%}"
-    print(f"{date:<10}{close:>10}{chgPct:>10}{vol:>15}{chgVol:>15}")
+    print(f"{date:<10}{close:>10}{chgPct:>20}{vol:>12}{chgVol:>10}")
 
 #day1 = f"{parsed['chart'][0]['close']}"
 #print(f"{price:>10}\n")
