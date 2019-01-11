eb6c4b2b4d7a78b30e873376f4461bf1748e7fad
edwins-acct <edwins.acct@gmail.com>
2019-01-11 17:25:16 +0000

    renamed history.py to stocks.py; created functions

---
+++ stocks.py
@@ -0,0 +1,103 @@
+#!python3
+
+import requests,json,sys,os,datetime, re
+from termcolor import colored
+from urllib.parse import urlparse, urlsplit, parse_qsl, parse_qs
+
+def quote_string_from_file(filename="aspire.txt"):
+    '''turns file into string of ticker symbols'''
+    with open(filename) as file:
+        fileContent = file.read()
+        string = fileContent.replace('\n',',')
+    return string;
+
+
+def get_data(url): 
+    '''query call'''
+    response = requests.get(url) #gets info 
+    if "Unknown symbol" in response.text: 
+        print(response.text) 
+        sys.exit() 
+    else: 
+        parse = urlparse(url) #prints> ParseResult(scheme='https', netloc='api.iextrading.com', path='/1.0/stock/market/batch', params='', query='symbols=V,FB,NBIX&types=quote', fragment='') querystring = parse_qs(parse.query)  #prints> {'symbols': ['V,FB,NBIX'], 'types': ['quote']} 
+        parsed = json.loads(response.text) #json.loads will parse data from response.text, has option to format. ex: json.loads(response.text, indent=4 
+        
+    #print(json.dumps(parsed_news, indent=2)) 
+    #print(len(parsed_news)) 
+    #print(parsed_news[0]['headline']) 
+    #sys.exit()
+
+    return parsed
+
+
+def history(qString): 
+    url= "https://api.iextrading.com/1.0/stock/" + str(qString.upper()) +"/batch?types=quote,chart&range=1m"
+    parsed = get_data(url)
+    print(f"{qString.upper() + '  Date':>10}{'Close':>10}{'Change $':>10}{'Change %':>10}{'Volume':>12}{'AvgVol':>10}") 
+    t_price = f"{parsed['quote']['latestPrice']:.2f}".join('$ ') 
+    x = str(parsed['quote']['change']) 
+    #x = str(parsed['quote']['changePercent']) 
+    if x.startswith("-"): 
+        t_chg= colored(f"{parsed['quote']['change']:.2f}".join('$ '),'red') 
+        t_chgPct = colored(f"{(parsed['quote']['changePercent']*100):.2f}".join(' %'),'red') #multiply by 100 before adding % 
+    else: 
+        t_chg= colored(f"{parsed['quote']['change']:.2f}".join('$ '),'green') 
+        t_chgPct = colored(f"{(parsed['quote']['changePercent']*100):.2f}".join(' %'),'green') #multiply by 100 before adding % 
+        
+    t_vol = parsed['quote']['latestVolume'] 
+    t_vol1 = f"{parsed['quote']['latestVolume']:,}" 
+    t_avgVol = parsed['quote']['avgTotalVolume'] 
+    t_chgVol = f"{(t_vol/t_avgVol):.0%}" 
+    print(f"{t_date:<10}{t_price:>10}{t_chg:>20}{t_chgPct:>20}{t_vol1:>12}{t_chgVol:>10}") 
+    #for i in range(0,len(parsed['chart'])): #in descending order 
+    for i in range((len(parsed['chart'])-1),-1,-1): #in ascending order 
+        date = f"{parsed['chart'][i]['date']}" 
+        close = f"{parsed['chart'][i]['close']:.2f}".join('$ ') 
+        x = str(parsed['chart'][i]['change']) #x = str(parsed['chart'][i]['changePercent']) 
+        if x.startswith("-"): 
+            chg= colored(f"{parsed['chart'][i]['change']:.2f}".join('$ '),'red') 
+            chgPct = colored(f"{parsed['chart'][i]['changePercent']:.2f}".join(' %'),'red') 
+        else: 
+            chg= colored(f"{parsed['chart'][i]['change']:.2f}".join('$ '),'green') 
+            chgPct = colored(f"{parsed['chart'][i]['changePercent']:.2f}".join(' %'),'green') 
+
+        chgPct.strip 
+        #    print(repr(chgPct))  #prints special characters;normally invisible 
+        vol= f"{parsed['chart'][i]['volume']:,}" 
+        chgVol = f"{(parsed['chart'][i]['volume']/t_avgVol):.0%}" 
+        print(f"{date:<10}{close:>10}{chg:>20}{chgPct:>20}{vol:>12}{chgVol:>10}")
+
+
+def news(qString): 
+    url= "https://api.iextrading.com/1.0/stock/" + str(qString.upper()) +"/news/last/5"
+    parsed = get_data(url)
+    print("\nNEWS:") 
+    for i in range(0, len(parsed)): 
+        date = parsed[int(i)]['datetime'] 
+        headline = parsed[int(i)]['headline'] 
+        source = colored(parsed[int(i)]['source'], 'yellow') 
+        url = colored(parsed[int(i)]['url'], 'blue') 
+        print(colored(date[:10], 'cyan')+"  "+headline+"  "+source+"\n   "+url)
+
+if __name__ == "__main__": 
+    x = datetime.datetime.now() 
+    rightNow = int(x.strftime("%H%M")) 
+    t_date = x.strftime("%Y-%m-%d") 
+    #rightMinute = int(rightNow.st) 
+    #print(rightNow)
+    # Check if command line input 
+    if len(sys.argv) > 1: 
+        if os.path.isfile(sys.argv[1]): 
+            qString = quote_string_from_file(sys.argv[1]) 
+        else: 
+            qString = ','.join(sys.argv[1:]) 
+    else: 
+        qString = quote_string_from_file()
+	
+
+    history(qString) 
+    news(qString) 
+    print("\n") 
+    
+    sys.exit()	
+
