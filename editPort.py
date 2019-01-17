9df88277c50ee25483045ae44fa052326dbd50d0
edwins-acct <edwins.acct@gmail.com>
2019-01-17 09:59:02 -0800

    update editPort.py to use db, stocks.db new schema for watchlist

--- editPort.py
+++ editPort.py
@@ -1,15 +1,27 @@
-import os, sys
+import os, sys, sqlite3,datetime
 
+user = input("user(sch_inv,fid_inv,fid_ira,fid_rira): ")
 name = input("name of portfolio: ")
-name += ".txt"
-if os.path.isfile(name): 
-    edit = input("add or delete: ")
-    edit = edit.upper()
-    x=set(line.strip() for line in open(name)) 
-
+#name += ".txt"
+#if os.path.isfile(name): 
+#    edit = input("add or delete: ")
+#    edit = edit.upper()
+#    x=set(line.strip() for line in open(name)) 
+#
 symbol = input("name of symbol(s): ")
 symbol = symbol.replace(',',' ').replace(';',' ').upper() #instead of using replace method 2x
 symbol=set(symbol.split())
+symbol=tuple(symbol)
+print(symbol)
+conn = sqlite3.connect('stocks.db')
+c = conn.cursor()
+c.executemany('INSERT INTO watchlist(user_id,w_name,symbol) VALUES (?,?,?)',symbol)
+conn.commit()
+conn.close()
+sys.exit()
+
+
+
 
 if os.path.isfile(name): 
     x=set(line.strip() for line in open(name)) 
@@ -29,3 +41,24 @@ with open(name,'w') as file:
 #import url
 #url
 #sys.exit()
+import sqlite3,fid_ira,fid_inv,fid_rira
+
+
+#c.execute('CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)')
+#CREATE TABLE user (id integer primary key autoincrement, user_id text unique not null, name text not null);
+#CREATE TABLE stock (id integer primary key autoincrement, symbol text unique not null, c_name text not null);
+#CREATE TABLE watchlist (id integer primary key autoincrement, user_id text not null, w_name text not null,symbol text not null);
+#CREATE TABLE history (id integer primary key autoincrement, user_id text not null,date text not null, trans text not null, symbol text not null, qty
+#         real, price real, foreign key(user_id) references user(user_id) on delete cascade, foreign key(symbol) references stock(symbol));
+
+# Insert a row of data
+ld=fid_ira.stocks.copy()
+lx=fid_rira.stocks.copy()
+lz=fid_inv.stocks.copy()
+c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lx)
+c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lz)
+
+# Save (commit) the changes
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
