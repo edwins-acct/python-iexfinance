bdac8a7bceea7040a1b1949777e7002722385b0c
edwins-acct <edwins.acct@gmail.com>
2019-01-17 14:23:40 +0000

    added db_connect.py to upload watchlists

--- db_connect.py
+++ db_connect.py
@@ -1,13 +1,22 @@
-import sqlite3
+import sqlite3,fid_ira,fid_inv,fid_rira
 
 conn = sqlite3.connect('/home/erobles1210/projects/python-iexfinance/stocks.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE stocks
-                     (date text, trans text, symbol text, qty real, price real)''')
+#c.execute('CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)')
+#CREATE TABLE user (id integer primary key autoincrement, user_id text unique not null, name text not null);
+#CREATE TABLE stock (id integer primary key autoincrement, symbol text unique not null, c_name text not null);
+#CREATE TABLE watchlist (id integer primary key autoincrement, user_id text not null, w_name text not null,symbol text not null);
+#CREATE TABLE history (id integer primary key autoincrement, user_id text not null,date text not null, trans text not null, symbol text not null, qty
+#         real, price real, foreign key(user_id) references user(user_id) on delete cascade, foreign key(symbol) references stock(symbol));
 
 # Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+ld=fid_ira.stocks.copy()
+lx=fid_rira.stocks.copy()
+lz=fid_inv.stocks.copy()
+c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',ld)
+c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lx)
+c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lz)
 
 # Save (commit) the changes
 conn.commit()
