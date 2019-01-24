8216917883369c65e0328fd0bf702cff9758974d
edwins-acct <edwins.acct@gmail.com>
2019-01-24 08:10:26 -0800

    updates to edit,stocks.db

--- editPort.py
+++ editPort.py
@@ -1,64 +1,55 @@
-import os, sys, sqlite3,datetime
+import sys, sqlite3, stocks
 
-user = input("user(sch_inv,fid_inv,fid_ira,fid_rira): ")
-name = input("name of portfolio: ")
-#name += ".txt"
-#if os.path.isfile(name): 
-#    edit = input("add or delete: ")
-#    edit = edit.upper()
-#    x=set(line.strip() for line in open(name)) 
-#
-symbol = input("name of symbol(s): ")
-symbol = symbol.replace(',',' ').replace(';',' ').upper() #instead of using replace method 2x
-symbol=set(symbol.split())
-symbol=tuple(symbol)
-print(symbol)
-conn = sqlite3.connect('stocks.db')
-c = conn.cursor()
-c.executemany('INSERT INTO watchlist(user_id,w_name,symbol) VALUES (?,?,?)',symbol)
-conn.commit()
+def user_inputs():
+	'''inputs will be used to query db'''
+	user = input("User_id(sch_inv,fid_inv,fid_ira,fid_rira): ")
+	name = input("Name of watchlist: ")
+	check_watchlist(user,name)
+
+
+def check_watchlist(user_id,w_name):
+	'''takes inputs from user_inputs and queries db'''
+	c.execute('SELECT symbol from watchlist where user_id=? and w_name=?',(user_id,w_name))
+	#use fetchone() because fetchall() returns empty tuple not 'None'
+	if c.fetchone() is None: 	
+		edit_watchlist(user_id,w_name)
+	else:
+		print(f'Current symbols in {w_name}:',end=' ')
+		for row in c.fetchall():
+			print(row[0],end=' ')
+
+		print('\n')
+		action = input("Add to or delete from watchlist: ")
+		edit_watchlist(user_id,w_name,action)
+
+def edit_watchlist(user_id,w_name,action="add"):
+	'''inputs from check_watchlist() and either adds or delete symbols'''
+	action = action.upper()
+	symbol = input("Stock symbol(s): ")
+	symbol = symbol.replace(',',' ').replace(';',' ').upper() #instead of using replace method 2x
+	symbol = set(symbol.split()) #create a set to eliminate dupes
+	input_list = () #create an empty tuple to use in loop
+	for sym in symbol:
+		input_list = input_list + ((user_id,w_name,sym),)		
+
+	#checks by getting first letter of action to minimize any user input errors; default is add
+	if action.startswith('D'):
+		c.executemany('DELETE FROM watchlist WHERE user_id=? and w_name=? and symbol=?',input_list)
+	else:
+		# use executemany for efficiency instead of loop; also use IGNORE so it doesn't terminate if duplicate in db
+		c.executemany('INSERT OR IGNORE INTO watchlist(user_id,w_name,symbol) VALUES (?,?,?)',input_list)
+	
+	conn.commit()
+	
+
+
+
+if __name__ == "__main__":
+	global conn
+	conn = stocks.db_connect('stocks.db')
+	c = conn.cursor()
+
+	user_inputs()
+
 conn.close()
 sys.exit()
-
-
-
-
-if os.path.isfile(name): 
-    x=set(line.strip() for line in open(name)) 
-    if edit.startswith('D'):
-        symbol = x - symbol
-    else: 
-        symbol.update(x) #use if you know both sets are exclusive otherwise use union
-
-symbol = sorted(symbol)
-y= " ".join(symbol)
-print("portfolio symbol(s): "+ y.upper())
-
-with open(name,'w') as file:
-    for sym in symbol:
-        file.write(sym.upper() + "\n")
-
-#import url
-#url
-#sys.exit()
-import sqlite3,fid_ira,fid_inv,fid_rira
-
-
-#c.execute('CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)')
-#CREATE TABLE user (id integer primary key autoincrement, user_id text unique not null, name text not null);
-#CREATE TABLE stock (id integer primary key autoincrement, symbol text unique not null, c_name text not null);
-#CREATE TABLE watchlist (id integer primary key autoincrement, user_id text not null, w_name text not null,symbol text not null);
-#CREATE TABLE history (id integer primary key autoincrement, user_id text not null,date text not null, trans text not null, symbol text not null, qty
-#         real, price real, foreign key(user_id) references user(user_id) on delete cascade, foreign key(symbol) references stock(symbol));
-
-# Insert a row of data
-ld=fid_ira.stocks.copy()
-lx=fid_rira.stocks.copy()
-lz=fid_inv.stocks.copy()
-c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lx)
-c.executemany('INSERT INTO history(user_id,date,trans,symbol,qty,price) VALUES (?,?,?,?,?,?)',lz)
-
-# Save (commit) the changes
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
--- stocks.py
+++ stocks.py
@@ -1,8 +1,56 @@
 #!python3
 
-import requests,json,sys,os,datetime, re
+import requests,json,sys,os,datetime, re, sqlite3
 from termcolor import colored
 from urllib.parse import urlparse, urlsplit, parse_qsl, parse_qs
+
+def db_connect(db="stocks.db"):
+    conn = sqlite3.connect(db)
+    return conn
+
+def user_inputs():
+    '''inputs will be used to query db'''
+    user = input("User_id(sch_inv,fid_inv,fid_ira,fid_rira): ")
+    name = input("Name of watchlist: ")
+    check_watchlist(user,name)
+
+
+def check_watchlist(user_id,w_name="watch"):
+    '''takes inputs from user_inputs and queries db'''
+    c.execute('SELECT symbol from watchlist where user_id=? and w_name=?',(user_id,w_name))
+    #use fetchone() because fetchall() returns empty tuple not 'None'
+    if c.fetchone() is None:    
+        edit_watchlist(user_id,w_name)
+    else:
+        print(f'Current symbols in {w_name}:',end=' ')
+        for row in c.fetchall():
+            print(row[0],end=' ')
+
+        print('\n')
+        action = input("Add to or delete from watchlist, return to skip: ")
+        if not action:
+            return
+        else:
+            edit_watchlist(user_id,w_name,action)
+
+def edit_watchlist(user_id,w_name,action="add"):
+    '''inputs from check_watchlist() and either adds or delete symbols'''
+    action = action.upper()
+    symbol = input("Stock symbol(s): ")
+    symbol = symbol.replace(',',' ').replace(';',' ').upper() #instead of using replace method 2x
+    symbol = set(symbol.split()) #create a set to eliminate dupes
+    input_list = () #create an empty tuple to use in loop
+    for sym in symbol:
+        input_list = input_list + ((user_id,w_name,sym),)       
+
+    #checks by getting first letter of action to minimize any user input errors; default is add
+    if action.startswith('D'):
+        c.executemany('DELETE FROM watchlist WHERE user_id=? and w_name=? and symbol=?',input_list)
+    else:
+        # use executemany for efficiency instead of loop; also use IGNORE so it doesn't terminate if duplicate in db
+        c.executemany('INSERT OR IGNORE INTO watchlist(user_id,w_name,symbol) VALUES (?,?,?)',input_list)
+    
+    conn.commit()
 
 def quote_string_from_file(filename="aspire.txt"):
     '''turns file into string of ticker symbols'''
@@ -10,7 +58,7 @@ def quote_string_from_file(filename="aspire.txt"):
         fileContent = file.read()
         string = fileContent.replace('\n',',')
     string = string[:-1] #deletes trailing comma in string
-    return string;
+    return string
 
 
 def get_data(url): 
